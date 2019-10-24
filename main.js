0c9b4a5d2866afe36a31102964fd2ff298969d13
ryokke <ryotahbb@icloud.com>
2019-10-24 19:46:16 +0900

    improve main.js

--- main.js
+++ main.js
@@ -14,7 +14,24 @@
  *         - username : 返信ツイートしたユーザー名(文字列。ユーザー名は自由につけて良い)
  *         - text : ツイート内容(文字列。ツイート内容は自由に作って良い。)
  */
-
+const tweets = [
+  {
+    username: "りょっけ",
+    text: "今日も頑張るぜ！",
+    replies: [
+      { username: "励ますマン", text: "頑張れ！" },
+      { username: "休めマン", text: "休め！" }
+    ]
+  },
+  {
+    username: "きょっけ",
+    text: "餃子が食べたい！",
+    replies: [
+      { username: "食べろマン", text: "食え！" },
+      { username: "やめろマン", text: "食うな！" }
+    ]
+  }
+];
 
 /**
  * 課題2: 課題1で作ったtweetsの内容を全て表示する
@@ -24,3 +41,13 @@
  *     - replies
  *       - repliesの内容もforEachを使って全て表示する
  */
+tweets.forEach((tweet, index) => {
+  console.log('--------ツイート' + index + '--------');
+  console.log('ユーザー名：', tweet.username);
+  console.log('ツイート：', tweet.text)
+  tweet.replies.forEach((reply, index) => {
+    console.log('---------返信' + index + "---------")
+    console.log('ユーザー名：', reply.username);
+    console.log('ツイート：', reply.text);
+  });
+});
